2965293b1a87b841be8a8061e3573b65467705bf
ktcotz <naskret.kamil@gmail.com>
2025-07-16 20:34:38 +0200

    feat: swagger
    Add swagger with config setup and dynamic version.

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { env } from './config/env';
+import { setupSwagger } from './swagger/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  setupSwagger(app);
+
   await app.listen(env.PORT);
 }
 bootstrap();
---
+++ apps/backend/src/swagger/swagger.constants.ts
@@ -0,0 +1,13 @@
+import { version } from './../../package.json';
+
+export const SWAGGER_CONFIG = {
+  title: 'Bibliotekarz',
+
+  description: 'Docs for Bibliotekarz app purpose!',
+
+  version: version,
+
+  path: 'api/docs',
+
+  bearerAuthName: 'Authorization',
+};
